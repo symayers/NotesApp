ddf2968dab06874e3d463d9f3c3dfe4657a7cfd6
Sydnee Mayers <symayers@microsoft.com>
2019-05-13 18:18:52 -0500

    Added additional testing capabilities

---
+++ AppiumTesting/AppiumDemo.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using System.Drawing.Imaging;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+
+namespace AppiumTesting
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        AppiumDriver<AndroidElement> driver;
+
+
+        
+        
+        /*public void BeforeAll()
+        {
+            
+        }*/
+
+        
+        /*public void AfterAll()
+        { 
+            driver.Quit();
+        }*/
+
+
+        [Test]
+        public void TestNotesApp()
+        {
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.SetCapability("platformName", "Android");
+            cap.SetCapability("deviceName", "pixel");
+            cap.SetCapability("platformVersion", "8.1");
+            // cap.SetCapability("udid", "358240051111110");
+            cap.SetCapability("noReset", "True");
+            cap.SetCapability("adbExecTimeout", "100000");
+            cap.SetCapability("app", "C:/Users/symayers/source/repos/Notes/Notes/Notes.Android/bin/Debug/com.companyname.Notes.apk");
+            driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), cap);
+            //driver.FindElementByClassName("android.widget.EditText");
+            AndroidElement editElement = driver.FindElementByClassName("android.widget.EditText");
+            editElement.SendKeys("New Notes");
+            AndroidElement saveElement = driver.FindElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup[1]/android.widget.Button");
+            saveElement.Click();
+            AndroidElement deleteElement = driver.FindElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup[2]/android.widget.Button");
+            deleteElement.Click();
+        }
+        
+    
+
+       
+    }
+}
---
+++ AppiumTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AppiumTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppiumTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c4fe2bf-6e2c-4a69-b03b-83e5c61eb2ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
