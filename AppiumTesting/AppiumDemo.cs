cd215eaf319eafa8a8c2885e534de500631c9e74
Sydnee Mayers <symayers@microsoft.com>
2019-05-15 08:08:07 -0500

    Updated Files

--- AppiumTesting/AppiumDemo.cs
+++ AppiumTesting/AppiumDemo.cs
@@ -1,6 +1,5 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Drawing.Imaging;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
@@ -22,33 +21,18 @@ namespace AppiumTesting
         AppiumDriver<AndroidElement> driver;
 
 
-        
-        
-        /*public void BeforeAll()
-        {
-            
-        }*/
-
-        
-        /*public void AfterAll()
-        { 
-            driver.Quit();
-        }*/
-
-
         [Test]
         public void TestNotesApp()
         {
             DesiredCapabilities cap = new DesiredCapabilities();
             cap.SetCapability("platformName", "Android");
-            cap.SetCapability("deviceName", "pixel");
+            cap.SetCapability("deviceName", "pixel2");
             cap.SetCapability("platformVersion", "8.1");
-            // cap.SetCapability("udid", "358240051111110");
             cap.SetCapability("noReset", "True");
-            cap.SetCapability("adbExecTimeout", "100000");
+            cap.SetCapability("adbExecTimeout", "1000000");
+            //cap.SetCapability("automationName", "UiAutomator2");
             cap.SetCapability("app", "C:/Users/symayers/source/repos/Notes/Notes/Notes.Android/bin/Debug/com.companyname.Notes.apk");
             driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), cap);
-            //driver.FindElementByClassName("android.widget.EditText");
             AndroidElement editElement = driver.FindElementByClassName("android.widget.EditText");
             editElement.SendKeys("New Notes");
             AndroidElement saveElement = driver.FindElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup[1]/android.widget.Button");
