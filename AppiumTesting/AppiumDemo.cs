71d0127b454cbd9e7d99d4a5692fc3a8dbf3956b
Sydnee Mayers <symayers@microsoft.com>
2019-05-15 08:17:05 -0500

    added warranty

--- AppiumTesting/AppiumDemo.cs
+++ AppiumTesting/AppiumDemo.cs
@@ -1,4 +1,16 @@
-﻿using NUnit.Framework;
+﻿/* Microsoft provides programming examples for illustration only, without warranty either expressed or
+ implied, including, but not limited to, the implied warranties of merchantability and/or fitness
+ for a particular purpose.
+
+ This sample assumes that you are familiar with the programming language being demonstrated and the
+ tools used to create and debug procedures. Microsoft support professionals can help explain the
+ functionality of a particular procedure, but they will not modify these examples to provide added
+ functionality or construct procedures to meet your specific needs. if you have limited programming
+ experience, you may want to contact a Microsoft Certified Partner or the Microsoft fee-based consulting
+ line at (800) 936-5200.*/
+
+
+using NUnit.Framework;
 using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
